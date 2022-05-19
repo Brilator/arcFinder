3ff2ffa71f06079ac384d58990012f60f4ab850b
Dominik <dominik.brilhaus@hhu.de>
2022-05-19 16:51:42 +0200

    bugfix installation

--- arcFinder/01_install_dependencies.R
+++ arcFinder/01_install_dependencies.R
@@ -8,6 +8,6 @@ required_packages <- c("tidyverse", "DBI", "shiny")
 for(package in required_packages)
 {
   ## Check if package is installed. If not, install
-  if(!require("readxl", quietly = TRUE)){install.packages(package)}
+  if(!require(package, quietly = TRUE)){install.packages(package)}
 }
 
